65614fb19dbc5813d0e574f64bd632e1755433c5
Matthew Wayock <wayock@gmail.com>
2019-03-02 19:43:26 -0500

    in <nav> changing Library to library

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <header>
           <nav>
             <Link to='/'>Landing</Link>
-            <Link to='/Library'>Library</Link>
+            <Link to='/library'>Library</Link>
           </nav>
           <h1>Bloc Jams</h1>
         </header>
