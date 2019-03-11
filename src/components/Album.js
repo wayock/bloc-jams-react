5f6bbb19c56bd0cc9cde32597bd023b91cfbc99a
Matthew Wayock <wayock@gmail.com>
2019-03-10 21:50:54 -0400

    Started playerbar checkpoint.  Up to Pass Down Data as Props section.

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
@@ -90,6 +91,7 @@ class Album extends Component {
           )}
           </tbody>
         </table>
+        <PlayerBar />
       </section>
     );
   }
