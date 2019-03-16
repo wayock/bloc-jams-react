a45046ddfe13ef8f4b333b0dada59afa0c91f2f2
Matthew Wayock <wayock@gmail.com>
2019-03-16 11:10:10 -0400

    started styling. pictures not displaying.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
+import pictures from  './src/pictures/';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
+          <nav className="navbar">
+            <Link className="nav-links" to='/'>Landing</Link>
+            <Link className="nav-links" to='/library'>Library</Link>
           </nav>
-          <h1>Bloc Jams</h1>
         </header>
-        <main>
-          <Route exact path="/" component={Landing} />
-          <Route path="/library" component={Library} />
-          <Route path="/album/:slug" component={Album} />
+        <main className="container">
+          <h1>Bloc Jams</h1>
+          <img src="/pictures/earbudsandcoffee.jpg" alt="Earbuds with coffee cup" />
+          <div className="container">
+            <Route exact path="/" component={Landing} />
+          </div>
+          <div class="container">
+            <Route path="/library" component={Library} />
+          </div>
+          <div class="container">
+            <Route path="/album/:slug" component={Album} />
+          </div>
         </main>
       </div>
     );
